d0e39d7b5ab03fc0ad37b54470a865db5844c66c
adrien <adrien.chaurin@etu.univ-rouen.fr>
2019-03-03 18:44:14 +0100

    game avec mauvais nom de fonction externe

--- Symfony/src/AGORA/Game/AugustusBundle/Model/AugustusGame.php
+++ Symfony/src/AGORA/Game/AugustusBundle/Model/AugustusGame.php
@@ -7,17 +7,58 @@ class AgustusGame extends Game {
         $em = $this->getDoctrine()->getManager();
         $game = $em->getRepository("AugustusBundle:AugustusGame")->find($id);
 
-        if ($game.isGameOver()) {
+        if ($game->isGameOver()) {
             return true;
         }
         //confirmation de nom
-        $this->setToken($board.getBagOfToken().takeToken());
+        $this->setToken($board->getBagOfToken()->takeToken());
 
-        $game.startGame($id);
+        $game->startGame($id);
+    }
+
+    public function drawToken() {
+        $this->board->getBagOfToken()->takeToken();
+    }
+
+    public function allOk() {
+        $ok = false;
+        foreach ($this->players as $player) {
+            $ok = $ok && $player->isLocked();
+        }
+        return $ok;
+    }
+
+    public function moveLegion($player,$source,$dest) {
+        if (!$player->historique) {
+            $player->putLegionFromSourceToDest($source, $this->token, $dest);
+        }
+    }
+
+    public function aveCesar($player) {
+        $cards = array();
+        foreach ($player->objectif() as $card) {
+            if ($card->toCapture() == 0) {
+                array_push($cards, $card);
+            }
+        }
+        foreach ($cards as $card) {
+            $player->increaseLegion($card->captured);
+            $card->doPower();
+            $player->captureObj($card);
+            $this->claimReward($player); //controlleur?
+            //recuperation de l'objectif (controlleur)
+            //pioche du nouvel objectif (controlleur)
+        }
+    }
+
+    public function claimReward($player) {
+        if(!$player->objAdvantage == 0) {
+            $player->objAdvantage = count($player->ctrlObj());
+        }
     }
 
     public function isGameOver($id){
-        foreach ($players as $player) {
+        foreach ($this->players as $player) {
             if (count($player.getDoneCards()) > 7) {
                 return true;
             }
