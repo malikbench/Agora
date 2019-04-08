35d37002d01295ec8c62ca888ad495058a08cfe5
Gatien <gatoun76@hotmail.fr>
2019-04-08 14:18:49 +0200

    Splendor jeu local
    Le jeu splendor fait en javascript en local par Deelan

---
+++ splendor/agora_splendor/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>splendor</title>
+        <meta charset="utf-8">
+        <link href="index_style.css" rel="stylesheet" type="text/css">
+
+    </head>
+
+<body>
+
+    <div class="label_III"> III </div>
+
+    <div class="label_II"> II </div>
+
+    <div class="label_I"> I </div>
+
+    /*1 div pour chaque joueur*/
+    <div class="player">
+        <span> player name</span>
+    </div>
+
+    <div class="cards">
+
+        <div class="box"><button>buy</button></div>
+        <div class="box"><button>buy</button></div>
+        <div class="box"><button>buy</button></div>
+        <div class="box"><button>buy</button></div>
+        <div class="box"><button>buy</button></div>
+        <div class="box"><button>buy</button></div>
+        <div class="box"><button>buy</button></div>
+        <div class="box"><button>buy</button></div>
+        <div class="box"><button>buy</button></div>
+        <div class="box"><button>buy</button></div>
+        <div class="box"><button>buy</button></div>
+        <div class="box"><button>buy</button></div>
+    </div>
+
+    <div class="tokens">
+        <div id="ruby" class="token"></div>
+        <div id="onyx" class="token"></div>
+        <div id="emerald" class="token"></div>
+        <div id="saphire" class="token"></div>
+        <div id="diamond" class="token"></div>
+        <div id="gold" class="token"></div>
+    </div>
+
+</body>
+</html>
