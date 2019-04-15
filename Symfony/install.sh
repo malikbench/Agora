1bdd6e08f2067e79115df06c96fa6bd31159b46f
Gatien <gatoun76@hotmail.fr>
2019-04-15 16:33:28 +0200

    Oublie du commit d'avant : ajout de la ligne executant le fichier sql
    Dans install.sh on ajoute la ligne permettant d'importer le fichier sql dans la BDD : php bin/console doctrine:database:import Data/SplendorCards.sql

--- Symfony/install.sh
+++ Symfony/install.sh
@@ -5,5 +5,6 @@ php composer.phar install
 ./updateSchema.sh
 php bin/console doctrine:database:import Data/GameInfo.sql
 php bin/console doctrine:database:import Data/admin.sql
+php bin/console doctrine:database:import Data/SplendorCards.sql
 ./clearCache.sh
 echo "Done !"
